7a901a498bddf939b0d97490fd12b7f0003dcebb
MauricioM06 <78988280+MauricioM06@users.noreply.github.com>
2021-12-01 20:42:21 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let Nome = prompt("Digite seu nome aqui:") || "";
+let Sobrenome = prompt("Digite seu sobrenome aqui:") || "";
+
+let NomeCompleto = `Nome Completo: ${nome} ${sobrenome}`;
+let NomeCatalogo = `Nome Catálago: ${sobrenome.toUpperCase()}, ${nome}`;
+
+alert(nomeCompleto);
+alert(nomeCatalogo);
